b978aa08ffb64778dce109d128101199c353d624
Andy Clyde <me@andyclyde.com>
2018-08-16 15:15:18 +0100

    Exercise 4

--- Section01/main.py
+++ Section01/main.py
@@ -1,3 +1,5 @@
+from string import Template
+
 def issue_greeting(name):
     return 'Hello, ' + name + ', nice to meet you!'
 
@@ -22,3 +24,8 @@ def get_quote_string(quote, author):
     assert len(quote) > 0
     assert len(author) > 0
     return author + " says \"" + quote + "\""
+
+
+def mad_libs(noun, verb, adjective, adverb):
+    mad_lib = Template('Do you $verb your $adjective $noun $adverb? So do I!')
+    return mad_lib.substitute(noun=noun, verb=verb, adjective=adjective, adverb=adverb)
---
+++ bin/exercise4.py
@@ -0,0 +1,8 @@
+from Section01.main import mad_libs
+
+noun = input('Enter a noun: ')
+verb = input('Enter a verb: ')
+adjective = input('Enter an adjective: ')
+adverb = input('Enter an adverb: ')
+
+print(mad_libs(noun, verb, adjective, adverb))
